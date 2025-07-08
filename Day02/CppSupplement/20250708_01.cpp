1ee6915913cce2ec809f7de502cd4ba7167eb8e2
hyungwoo1413 <glhwguddn@gmail.com>
2025-07-08 19:51:19 +0900

    커밋

---
+++ Day02/CppSupplement/20250708_01.cpp
@@ -0,0 +1,56 @@
+﻿#include <stdio.h>
+
+int a;
+const int global_c = 100;
+
+int func2() {
+	return 1;
+}
+
+void func() {
+	printf("function : %p\n", func);		// 함수의 이름 = 주소
+}											// 배열의 이름 = 주소			
+
+int main()
+{
+	int b = 100;
+	static int c;
+	const int d = 10;	// 선언과 동시에 초기화 시켜야 함.	
+						// 변수 d를 통해서 d의 값을 변경 x
+	char arr[20] = "Hi";
+	// d = 20;
+	/*const int* pd = &d;
+
+	const int* pb;
+	pb = &d;
+
+	int c =  200;
+	int* const ppb = &b;
+
+	*ppb = 1000;
+	ppb = &c;
+
+	const int* const ppc = &c;*/
+
+	// *pb = 10;
+	// b = 10;
+
+
+	func();
+	printf("func2() : %p\n", func2);
+	printf("local b : %p\n", &b);
+	printf("local c : %p\n", &c);
+	printf("const d : %p\n", &d);
+	printf("array : %p\n", arr);
+	printf("global a : %p\n", &a);
+	printf("global c : %p\n", &global_c);
+
+	return 0;
+}
+
+/*
+	변수 : 무언가을 저장할 수 있는 공간 
+
+	data segment : stack(지역, 매개변수), heap(사용자), data(전역, static, 문자열 리터럴)`
+	code segment : 함수 하나당 한개 할당
+*/
---
+++ Day02/CppSupplement/20250708_02.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+void change_val(int* p)
+{
+	*p = 20; // 포인터를 통해서 값 변경
+}
+
+int main()
+{
+	int num = 10;
+	std::cout << num << std::endl;
+	change_val(&num);
+	std::cout << num << std::endl;
+
+	return 0;
+}
+
+void change_val(int& p)
+{
+	p = 20;
+}
+
+int main()
+{
+	int num = 10;
+	std::cout << num << std::endl;
+	change_val(num);
+	std::cout << num << std::endl;
+
+	return 0;
+}
+
+
+/*
+	int n;
+	int* pn;
+	int& rn;	// 레퍼런스 변수(별명, 별칭)
+
+	(lvalue)int n = 10(rvalue : 리터럴);
+	n = n2;
+	10 = n2; X
+*/
